4d95cd28333b4aff2555e0af4d8b702fe0999f39
bhooshanmate <bhooshanmate@gmail.com>
2024-06-30 20:31:55 +0530

    wrote concrete class/implementation for magical arena

--- src/main/java/SwiggyAsde/model/MagicalArenaImpl.java
+++ src/main/java/SwiggyAsde/model/MagicalArenaImpl.java
@@ -2,14 +2,37 @@ package SwiggyAsde.model;
 
 import SwiggyAsde.Player;
 
+import java.util.Random;
+
 public class MagicalArenaImpl implements MagicalArena{
+    Random randomNumber = new Random();
+
     @Override
     public Integer rollDice() {
-        return 0;
+//        1 to 6 sided dice
+        return randomNumber.nextInt(6) + 1;
     }
 
     @Override
-    public void fight(Player player1, Player player2) {
+    public void fight(Player attacker, Player defender) {
+        // rolling for both the players
+        Integer attackRoll = rollDice();
+        Integer defenseRoll = rollDice();
 
+        // calculating attack damage & defense strength
+        Integer attackDamage = attacker.getAttack() * attackRoll;
+        Integer defenseStrength = defender.getStrength() * defenseRoll;
+
+        // calculating damage taken by the player
+        // & the remaining health of that player
+        Integer damageTaken = Math.max(0,attackDamage - defenseStrength);
+        Integer remainingHealth = defender.getHealth() - damageTaken;
+
+        // setting up the health
+        defender.setHealth(remainingHealth);
+
+        System.out.println("Attack rolls : " + attackRoll + ", Defender roll : " + defenseRoll);
+        System.out.println("Attack damage : " + attackDamage + ", Defense strength : " + defenseStrength);
+        System.out.println("Defender health reduced by : " + damageTaken + " to" + defender.getHealth());
     }
 }
