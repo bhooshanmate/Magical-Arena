c1cd461da31d7f1555d3379fcd31959d67a3109a
bhooshanmate <bhooshanmate@gmail.com>
2024-06-30 19:58:58 +0530

    made entity class - Player

---
+++ src/main/java/SwiggyAsde/Player.java
@@ -0,0 +1,15 @@
+package SwiggyAsde;
+
+public class Player {
+    private Integer health;
+    private Integer strength;
+    private Integer attack;
+
+    public Player(Integer health, Integer strength, Integer attack) {
+        this.health = health;
+        this.attack = attack;
+        this.strength = strength;
+    }
+}
+
+
